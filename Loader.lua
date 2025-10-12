28289ad475866ae2ec5f83033fe1649559ae9c0e
Hzddprivate <148460926+Hzddprivate@users.noreply.github.com>
2025-10-12 23:00:23 +0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,8 @@
+local GameId = game.GameId
+
+local GameList = {
+    [994732206] = "Blox%20Fruits/Loader.lua",
+    [6765805766] = "Block%20Spin/Loader.lua",
+}
+
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/Hzddprivate/Script/main/%s"):format(GameList[GameId])))()
