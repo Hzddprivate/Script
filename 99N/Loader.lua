b9e939f42e826622ca4f46f6f778ccadf3739247
Hzddprivate <148460926+Hzddprivate@users.noreply.github.com>
2025-10-14 16:45:18 +0700

    Create Loader.lua

---
+++ 99N/Loader.lua
@@ -0,0 +1,3 @@
+(getgenv()).key = "MARU0RMFMYMFPXOI6YYZUNUM";
+(getgenv()).id = "878872687286571018";
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruComkak/main/PCBit.lua"))()
