8425c2266434220de5c3bcabdc185393028e469e
Hzddprivate <148460926+Hzddprivate@users.noreply.github.com>
2025-10-15 09:49:30 +0700

    Update Loader.lua

--- Block Spin/Loader.lua
+++ Block Spin/Loader.lua
@@ -1,1 +1,8 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/hermanos-dev/hermanos-hub/refs/heads/main/BlockSpin/blockspin-farm.lua'))()
+--[[
+Credit: Hermanos-Dev, Dont Delete This.
+]]--
+
+local loader = loadstring
+local url = "https://raw.githubusercontent.com/hermanos-dev/hermanos-hub/refs/heads/main/BlockSpin/blockspin-pvp.lua"
+local response = game:HttpGet(url)
+loader(response)()
