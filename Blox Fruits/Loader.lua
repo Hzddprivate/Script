f08a6989dc68fb914d750fe493345c75b0800af6
Hzddprivate <148460926+Hzddprivate@users.noreply.github.com>
2025-10-16 20:16:06 +0700

    Update Loader.lua

--- Blox Fruits/Loader.lua
+++ Blox Fruits/Loader.lua
@@ -5,28 +5,33 @@ repeat task.wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui")
 setfpscap(15)
 getgenv().Mode = "OneClick"
 getgenv().Setting = {
-    ["Team"] = "Pirates", -- Options "Pirates", "Marines"
+    ["Team"] = "Pirates",
     ["FucusOnLevel"] = true,
-    ["Fruits"] = {  -- setting for fruits u want
-        ["Primary"] = { -- if current fruit is not in this list, eat/buy
+    ["Fruits"] = {  
+        ["Primary"] = { 
             "Dough-Dough",
             "Dragon-Dragon",
             "Buddha-Buddha",
-            -- u can configs add mores/remove and must end with , (comma symbol)
         },
-        ["Normal"] = { -- it just a normal fruit list
-            "Flame-Flame",
-            "Light-Light",
+        ["Normal"] = { 
+            "Dark-Dark",
+            "Quake-Quake",
             "Magma-Magma",
-            -- u can configs add mores/remove and must end with , (comma symbol)
         }
-        -- run this for get all fruit name `local t={};for _,v in pairs(game.ReplicatedStorage.Remotes.CommF_:InvokeServer("GetFruits"))do table.insert(t,v.Name)end;setclipboard(table.concat(t, "\n"))`
     },
     ["Lock Fruits"] = { -- don't use or eat fruits in this list
         "Yeti-Yeti",
         "T-Rex-T-Rex"
+        "Kitsune-Kitsune",
+        "Leopard-Leopard",
+        "Mammoth-Mammoth",
+        "Dough-Dough",
+        "Gas-Gas",
+        "Spirit-Spirit",
+        "Gravity-Gravity",
+        "Venom-Venom"
     },
-    ["IdleCheck"] = 300, -- every (x) seconds if not moving rejoin
+    ["IdleCheck"] = 150, -- every (x) seconds if not moving rejoin
 };
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xQuartyx/QuartyzScript/main/Loader.lua"))()
