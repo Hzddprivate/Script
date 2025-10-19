75a8896d0013470bc8f7b2f7eca40b5566d38afd
Hzddprivate <148460926+Hzddprivate@users.noreply.github.com>
2025-10-19 15:37:39 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,7 @@
+repeat task.wait() until game:IsLoaded()
+repeat task.wait() until game.Players
+repeat task.wait() until game.Players.LocalPlayer
+repeat task.wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui")
 -- ====== Loader.lua ======
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
