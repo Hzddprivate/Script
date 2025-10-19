a8af94fe4ab1d9178b2bdcd5263e1161e8e31278
Hzddprivate <148460926+Hzddprivate@users.noreply.github.com>
2025-10-19 15:37:14 +0700

    Update Loader.lua

--- Blox Fruits/Loader.lua
+++ Blox Fruits/Loader.lua
@@ -1,3 +1,7 @@
+repeat task.wait() until game:IsLoaded()
+repeat task.wait() until game.Players
+repeat task.wait() until game.Players.LocalPlayer
+repeat task.wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui")
 getgenv().Mode = "OneClick"
 getgenv().Setting = {
     ["Team"] = "Pirates",
