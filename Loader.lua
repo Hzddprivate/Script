4e828d6d65c536917cb3a13f89702c4132a74516
Hzddprivate <148460926+Hzddprivate@users.noreply.github.com>
2025-10-23 20:52:40 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -5,4 +5,4 @@ local GameList = {
     [6765805766] = "Block%20Spin/Loader.lua",
 }
 
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/Hzddprivate/Script/main/%s"):format(GameList[GameId])))(
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/Hzddprivate/Script/main/%s"):format(GameList[GameId])))()
