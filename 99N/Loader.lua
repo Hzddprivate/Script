66e209dfe8e8193c7e74cdc0e0df04b32dc1490d
Hzddprivate <148460926+Hzddprivate@users.noreply.github.com>
2025-10-25 18:54:26 +0700

    Update Loader.lua

--- 99N/Loader.lua
+++ 99N/Loader.lua
@@ -1,1 +1,7 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/collonroger/pigeonhub/refs/heads/main/autofarmdiamonds.lua"))()
+getgenv().V = "Kaitundiamond" --ConfigKaitun/คอนฟิกไก่ตัน
+getgenv().Webhookurl = "webhook" --linkwebhook/ลิ้งเว็ปฮุก
+getgenv().Day = 100 --Day To Playagain / วันที่จะเริ่มใหม่
+getgenv().SetFpsCap  = 999
+getgenv().WhiteScreen = true  -- WhiteScrren/จอขาว
+script_key="ZppBGQfHbWkXZMjoLinghvDSXcathXsy";
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AAwful/Vector_Hub/0/v2"))()
