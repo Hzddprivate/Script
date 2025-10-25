9b8e9daebc7c178bd8c595202e840f423fc07379
Hzddprivate <148460926+Hzddprivate@users.noreply.github.com>
2025-10-25 17:56:31 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,21 @@
+repeat task.wait() until game:IsLoaded()
+
+local executorname = "Unknown"
+if getexecutorname then
+    executorname = getexecutorname()
+elseif identifyexecutor then
+    executorname = identifyexecutor()
+end
+
+local FAKE_EXECUTOR = {  }
+
+for i,v in pairs(FAKE_EXECUTOR) do
+    if executorname:lower():find(v) then
+        game.Players.LocalPlayer:Kick("\n\n" .. executorname .. " is not supported")
+        return
+    end
+end
+
 local GameId = game.GameId
 
 local GameList = {
