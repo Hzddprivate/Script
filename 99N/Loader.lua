673cab07de62c5dcf9560f7ae933ab8e58afd35f
Hzddprivate <148460926+Hzddprivate@users.noreply.github.com>
2025-10-25 17:54:25 +0700

    Update Loader.lua

--- 99N/Loader.lua
+++ 99N/Loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/caomod2077/Script/main/FoxnameHub.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/collonroger/pigeonhub/refs/heads/main/autofarmdiamonds.lua"))()
