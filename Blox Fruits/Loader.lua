b2f25320d2a9ba52a5e9c9ff7c246ff086a35cfa
Hzddprivate <148460926+Hzddprivate@users.noreply.github.com>
2025-10-26 19:34:14 +0700

    Update Loader.lua

--- Blox Fruits/Loader.lua
+++ Blox Fruits/Loader.lua
@@ -2,11 +2,25 @@ repeat task.wait() until game:IsLoaded()
 repeat task.wait() until game.Players
 repeat task.wait() until game.Players.LocalPlayer
 repeat task.wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui")
-setfpscap(15)
-_G.Team = "Pirate" -- Marine / Pirate
-getgenv().Script_Mode = "Kaitun_Script"
-_G.OneClickSettings = {
-    ["LockFruits"] = {
+getgenv().Mode = "OneClick"
+getgenv().Setting = {
+    ["Team"] = "Pirates", -- Options "Pirates", "Marines"
+    ["FucusOnLevel"] = true,
+    ["Fruits"] = {  -- setting for fruits u want
+        ["Primary"] = { -- if current fruit is not in this list, eat/buy
+            "Dough-Dough",
+            "Dragon-Dragon",
+            "Buddha-Buddha",
+            -- u can configs add mores/remove and must end with , (comma symbol)
+        },
+        ["Normal"] = { -- it just a normal fruit list
+            "Dark-Dark",
+            "Magma-Magma",
+            -- u can configs add mores/remove and must end with , (comma symbol)
+        }
+        -- run this for get all fruit name `local t={};for _,v in pairs(game.ReplicatedStorage.Remotes.CommF_:InvokeServer("GetFruits"))do table.insert(t,v.Name)end;setclipboard(table.concat(t, "\n"))`
+    },
+    ["Lock Fruits"] = { -- don't use or eat fruits in this list
         "Dough-Dough",
         "Dragon-Dragon",
         "Yeti-Yeti",
@@ -18,16 +32,7 @@ _G.OneClickSettings = {
         "T-Rex-T-Rex",
         "Mammoth-Mammoth"
     },
-    ["MainFruits"] = {
-        "Dough-Dough",
-        "Buddha-Buddha"
-    },
-    ["RegularFruits"] = {
-        "Magmag-Magma",
-        "Dark-Dark"
-    },
-    ["FragmentsCap"] = 50000
-}
-(getgenv()).key = "MARU0RMFMYMFPXOI6YYZUNUM";
-(getgenv()).id = "878872687286571018";
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruComkak/main/PCBit.lua"))()
+    ["IdleCheck"] = 150, -- every (x) seconds if not moving rejoin
+};
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xQuartyx/QuartyzScript/main/Loader.lua"))()
