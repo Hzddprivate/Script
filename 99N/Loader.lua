f86e210e6deac9cc8149ea5c6a92a529089cc296
Hzddprivate <148460926+Hzddprivate@users.noreply.github.com>
2025-10-27 21:02:27 +0700

    Update Loader.lua

--- 99N/Loader.lua
+++ 99N/Loader.lua
@@ -1,7 +1,19 @@
-getgenv().V = "Kaitundiamond" --ConfigKaitun/คอนฟิกไก่ตัน
-getgenv().Webhookurl = "webhook" --linkwebhook/ลิ้งเว็ปฮุก
-getgenv().Day = 100 --Day To Playagain / วันที่จะเริ่มใหม่
-getgenv().SetFpsCap  = 999
-getgenv().WhiteScreen = true  -- WhiteScrren/จอขาว
-script_key="ZppBGQfHbWkXZMjoLinghvDSXcathXsy";
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AAwful/Vector_Hub/0/v2"))()
+repeat task.wait() until game:IsLoaded()
+_G.WebhookLink = "https://discord.com/api/webhooks/1420746655820681216/zV6UA7Gcddtr2k4Pycku89W571UNs5FY64hmtIk4mTGkkPw_yRUnCSU5FydVfCco_5BY"
+_G.OPFarmGem = true
+
+_G.Class = {
+    Enabled = true,
+    Target = "Cyborg" -- Class Name or Target = {"Gambler" , "Cyborg"}
+}
+
+_G.LockDiamond = {
+    Enabled = true,
+    Amount = 1000, -- 1000,2000,3000
+    SendWebhook = {
+        Enabled = true,
+        WebhookLink = "https://discord.com/api/webhooks/1420746655820681216/zV6UA7Gcddtr2k4Pycku89W571UNs5FY64hmtIk4mTGkkPw_yRUnCSU5FydVfCco_5BY",
+        Message = "Reached Target", 
+    }
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NAVAAI098/Thunder-Hub/main/Kaitun.lua"))()
