8f35883191e3597e8f25940a07efa535ecadef44
Hzddprivate <148460926+Hzddprivate@users.noreply.github.com>
2025-11-02 13:38:20 +0700

    Update Loader.lua

--- Blox Fruits/Loader.lua
+++ Blox Fruits/Loader.lua
@@ -102,4 +102,6 @@ _G.FarmMastery_Settings = {
 _G.Hop_Settings = {
     ["Find Tushita"] = true
 }
--- Script Here !!!
+(getgenv()).key = "MARU0RMFMYMFPXOI6YYZUNUM";
+(getgenv()).id = "878872687286571018";
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruComkak/main/PCBit.lua"))()
