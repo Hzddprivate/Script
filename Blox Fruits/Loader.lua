bbfac4895682de7002222f0925c14c99cd6e236a
Hzddprivate <148460926+Hzddprivate@users.noreply.github.com>
2025-11-04 18:48:49 +0700

    Update Loader.lua

--- Blox Fruits/Loader.lua
+++ Blox Fruits/Loader.lua
@@ -60,7 +60,7 @@ getgenv().SettingFarm = {
     },
     ["Farm Mastery"] = {
         ["Melee"] = true,
-        ["Sword"] = false,
+        ["Sword"] = true,
     },
     ["Buy Haki"] = {
         ["Enhancement"] = true,
