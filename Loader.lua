362d9af8cc00f0a2b1c563846a3a73618bce6e34
Hzddprivate <148460926+Hzddprivate@users.noreply.github.com>
2025-11-04 19:36:46 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -19,9 +19,9 @@ end
 local GameId = game.GameId
 
 local GameList = {
-    [994732206] = { "Blox%20Fruits/Loader.lua", "Blox%20Fruits/Horst.lua" },
-    [7326934954] = { "99N/Loader.lua", "99N/Horst.lua" },
-    [79546208627805] = { "99N/Loader.lua", "99N/Horst.lua" },
+    [994732206] = {"Blox%20Fruits/Loader.lua", "Blox%20Fruits/Horst.lua"},
+    [7326934954] = {"99N/Loader.lua", "99N/Horst.lua"},
+    [79546208627805] = {"99N/Loader.lua", "99N/Horst.lua"},
 }
 
 loadstring(game:HttpGet(("https://raw.githubusercontent.com/Hzddprivate/Script/main/%s"):format(GameList[GameId])))()
