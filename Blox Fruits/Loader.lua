184bf03d03d22e5f7dacd0a10a48817d0b67f3ba
Hzddprivate <148460926+Hzddprivate@users.noreply.github.com>
2025-11-05 18:21:48 +0700

    Update Loader.lua

--- Blox Fruits/Loader.lua
+++ Blox Fruits/Loader.lua
@@ -1,87 +1,29 @@
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
-getgenv().Key = "24e26bff48554cdacae1f244"
-getgenv().SettingFarm = {
-    ["Multiple Attack"] = true,
-    ["Auto Buso"] = true,
-    ["Hide UI"] = false,
-    ["Reset Teleport"] = {
-        ["Enabled"] = false,
-        ["Delay Reset"] = 3,
-        ["Item Dont Reset"] = {
-            ["Fruit"] = {
-                ["Enabled"] = true,
-                ["All Fruit"] = true,
-                ["Select Fruit"] = {
-                    ["Enabled"] = false,
-                    ["Fruit"] = {},
-                },
-            },
-        },
-    },
-    ["White Screen"] = false,
-    ["Lock Fps"] = {
-        ["Enabled"] = true,
-        ["FPS"] = 15,
-    },
-    ["Get Items"] = {
-        ["Saber"] = true,
-        ["Godhuman"] = true,
-        ["Skull Guitar"] = false,
-        ["Mirror Fractal"] = true,
-        ["Cursed Dual Katana"] = true,
-        ["Upgrade Race V2-V3"] = false,
-        ["Auto Pull Lever"] = true,
-        ["Shark Anchor"] = false,
-    },
-    ["Get Rare Items"] = {
-        ["Rengoku"] = true,
-        ["Dragon Trident"] = true,
-        ["Pole (1st Form)"] = true,
-        ["Gravity Blade"] = true,
-    },
-    ["Farm Fragments"] = {
-        ["Enabled"] = true,
-        ["Fragment"] = 50000,
-    },
-    ["Auto Chat"] = {
-        ["Enabled"] = false,
-        ["Text"] = "Config that cut tay",
-    },
-    ["Auto Summon Rip Indra"] = true,
-    ["Select Hop"] = {
-        ["Hop Server If Have Player Near"] = false,
-        ["Hop Find Rip Indra Get Valkyrie Helm or Get Tushita"] = true,
-        ["Hop Find Dough King Get Mirror Fractal"] = true,
-        ["Hop Find Raids Castle [CDK]"] = true,
-        ["Hop Find Cake Queen [CDK]"] = false,
-        ["Hop Find Soul Reaper [CDK]"] = true,
-        ["Hop Find Darkbeard [SG]"] = false,
-        ["Hop Find Mirage [ Pull Lever ]"] = false,
-    },
-    ["Farm Mastery"] = {
-        ["Melee"] = true,
-        ["Sword"] = true,
-    },
-    ["Buy Haki"] = {
-        ["Enhancement"] = true,
-        ["Skyjump"] = true,
-        ["Flash Step"] = true,
-        ["Observation"] = true,
-    },
-    ["Sniper Fruit Shop"] = {
-        ["Enabled"] = true, -- Auto Buy Fruit in Shop Mirage and Normal
-        ["Fruit"] = {
+getgenv().Mode = "OneClick"
+getgenv().Setting = {
+    ["Team"] = "Pirates", -- Options "Pirates", "Marines"
+    ["FucusOnLevel"] = true,
+    ["Fruits"] = {  -- setting for fruits u want
+        ["Primary"] = { -- if current fruit is not in this list, eat/buy
+            "Dough-Dough",
             "Tiger-Tiger",
             "Kitsune-Kitsune",
             "Dragon-Dragon",
             "Yeti-Yeti",
             "Gas-Gas",
-            "Dough-Dough",
             "Buddha-Buddha",
             "Spirit-Spirit",
+            "Buddha-Buddha",
         },
+        ["Normal"] = { -- it just a normal fruit list
+            "Dark-Dark",
+            "Light-Light",
+            "Magma-Magma",
+            -- u can configs add mores/remove and must end with , (comma symbol)
+        }
+        -- run this for get all fruit name `local t={};for _,v in pairs(game.ReplicatedStorage.Remotes.CommF_:InvokeServer("GetFruits"))do table.insert(t,v.Name)end;setclipboard(table.concat(t, "\n"))`
     },
-    ["Lock Fruit"] = {
+    ["Lock Fruits"] = { -- don't use or eat fruits in this list
         "Tiger-Tiger",
         "Kitsune-Kitsune",
         "Dragon-Dragon",
@@ -89,15 +31,9 @@ getgenv().SettingFarm = {
         "Gas-Gas",
         "Dough-Dough",
         "Buddha-Buddha",
-        "Spirit-Spirit",
+        "Spirit-Spirit"
     },
-    ["Webhook"] = {
-        ["Enabled"] = false,
-        ["WebhookUrl"] = "",
-    },
-}
+    ["IdleCheck"] = 150, -- every (x) seconds if not moving rejoin
+};
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaCat-kaitunBF.lua"))()
-task.delay(6, function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Hzddprivate/Script/main/Blox%20Fruits/MoreConfig.lua"))()
-end)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xQuartyx/QuartyzScript/main/Loader.lua"))()
