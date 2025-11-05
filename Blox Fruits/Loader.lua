31010b4bfeccd0191abeb5e5971a3106cfd6b74b
Hzddprivate <148460926+Hzddprivate@users.noreply.github.com>
2025-11-05 20:00:25 +0700

    Update Loader.lua

--- Blox Fruits/Loader.lua
+++ Blox Fruits/Loader.lua
@@ -1,105 +1,103 @@
-repeat task.wait() until game:IsLoaded()
-repeat task.wait() until game.Players
-repeat task.wait() until game.Players.LocalPlayer
-repeat task.wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui")
-_G.Team = "Pirate" -- Marine / Pirate
-getgenv().Script_Mode = "Kaitun_Script"
-_G.MainSettings = {
-    ["EnabledHOP"] = true,
-    ['FPSBOOST'] = true,
-    ["FPSLOCKAMOUNT"] = 15,
-    ['WhiteScreen'] = false,
-    ['CloseUI'] = false,
-    ["NotifycationExPRemove"] = true,
-    ['AFKCheck'] = 150,
-    ["LockFragments"] = 50000,
-    ["LockFruitsRaid"] = {
-        [1] = "Tiger-Tiger",
-        [2] = "Kitsune-Kitsune",
-        [3] = "Dragon-Dragon",
-        [4] = "Yeti-Yeti",
-        [5] = "Gas-Gas",
-        [6] = "Dough-Dough",
-        [7] = "Buddha-Buddha",
-        [8] = "Spirit-Spirit",
-    }
-}
-_G.SharkAnchor_Settings = {
-    ["Enabled_Farm"] = false,
-    ['FarmAfterMoney'] = 2500000
-}
-_G.Quests_Settings = {        
-    ['Rainbow_Haki'] = true,
-    ["MusketeerHat"] = true,
-    ["PullLever"] = true,
-    ['DoughQuests_Mirror'] = {
-        ['Enabled'] = true,
-        ['UseFruits'] = true
-    }        
-}
-_G.Races_Settings = {
-    ['Race'] = {
-        ['EnabledEvo'] = true,
-        ["v2"] = true,
-        ["v3"] = true,
-        ["Races_Lock"] = {
-            ["Races"] = {
-                ["Mink"] = true,
-                ["Human"] = true,
-                ["Fishman"] = true
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
+getgenv().Key = "24e26bff48554cdacae1f244"
+getgenv().SettingFarm = {
+    ["Multiple Attack"] = true,
+    ["Auto Buso"] = true,
+    ["Hide UI"] = false,
+    ["Reset Teleport"] = {
+        ["Enabled"] = false,
+        ["Delay Reset"] = 3,
+        ["Item Dont Reset"] = {
+            ["Fruit"] = {
+                ["Enabled"] = true,
+                ["All Fruit"] = true,
+                ["Select Fruit"] = {
+                    ["Enabled"] = false,
+                    ["Fruit"] = {},
+                },
             },
-            ["RerollsWhenFragments"] = 20000
-        }
-    }
+        },
+    },
+    ["White Screen"] = false,
+    ["Lock Fps"] = {
+        ["Enabled"] = true,
+        ["FPS"] = 15,
+    },
+    ["Get Items"] = {
+        ["Saber"] = true,
+        ["Godhuman"] = true,
+        ["Skull Guitar"] = false,
+        ["Mirror Fractal"] = true,
+        ["Cursed Dual Katana"] = true,
+        ["Upgrade Race V2-V3"] = false,
+        ["Auto Pull Lever"] = true,
+        ["Shark Anchor"] = false,
+    },
+    ["Get Rare Items"] = {
+        ["Rengoku"] = true,
+        ["Dragon Trident"] = true,
+        ["Pole (1st Form)"] = true,
+        ["Gravity Blade"] = true,
+    },
+    ["Farm Fragments"] = {
+        ["Enabled"] = true,
+        ["Fragment"] = 50000,
+    },
+    ["Auto Chat"] = {
+        ["Enabled"] = false,
+        ["Text"] = "Config that cut tay",
+    },
+    ["Auto Summon Rip Indra"] = true,
+    ["Select Hop"] = {
+        ["Hop Server If Have Player Near"] = false,
+        ["Hop Find Rip Indra Get Valkyrie Helm or Get Tushita"] = true,
+        ["Hop Find Dough King Get Mirror Fractal"] = true,
+        ["Hop Find Raids Castle [CDK]"] = true,
+        ["Hop Find Cake Queen [CDK]"] = true,
+        ["Hop Find Soul Reaper [CDK]"] = true,
+        ["Hop Find Darkbeard [SG]"] = false,
+        ["Hop Find Mirage [ Pull Lever ]"] = false,
+    },
+    ["Farm Mastery"] = {
+        ["Melee"] = true,
+        ["Sword"] = true,
+    },
+    ["Buy Haki"] = {
+        ["Enhancement"] = true,
+        ["Skyjump"] = true,
+        ["Flash Step"] = true,
+        ["Observation"] = true,
+    },
+    ["Sniper Fruit Shop"] = {
+        ["Enabled"] = true, -- Auto Buy Fruit in Shop Mirage and Normal
+        ["Fruit"] = {
+            "Tiger-Tiger",
+            "Kitsune-Kitsune",
+            "Dragon-Dragon",
+            "Yeti-Yeti",
+            "Gas-Gas",
+            "Dough-Dough",
+            "Buddha-Buddha",
+            "Spirit-Spirit",
+        },
+    },
+    ["Lock Fruit"] = {
+        "Tiger-Tiger",
+        "Kitsune-Kitsune",
+        "Dragon-Dragon",
+        "Yeti-Yeti",
+        "Gas-Gas",
+        "Dough-Dough",
+        "Buddha-Buddha",
+        "Spirit-Spirit",
+    },
+    ["Webhook"] = {
+        ["Enabled"] = false,
+        ["WebhookUrl"] = "",
+    },
 }
-_G.Fruits_Settings = {
-    ['Main_Fruits'] = {'Dough-Dough', 'Buddha-Buddha'},
-    ['Select_Fruits'] = {"Light-Light", "Dark-Dark", "Magma-Magma"}
-}
-_G.Settings_Melee = {
-    ['Superhuman'] = true,
-    ['DeathStep'] = true,
-    ['SharkmanKarate'] = true,
-    ['ElectricClaw'] = true,
-    ['DragonTalon'] = true,
-    ['Godhuman'] = true
-}
-_G.SwordSettings = {
-    ['Saber'] = true,
-    ["Pole"] = true,
-    ['MidnightBlade'] = true,
-    ['Shisui'] = true,
-    ['Saddi'] = false,
-    ['Wando'] = false,
-    ['Yama'] = true,
-    ['Rengoku'] = true,
-    ['Canvander'] = true,
-    ['BuddySword'] = true,
-    ['TwinHooks'] = true,
-    ['HallowScryte'] = true,
-    ['TrueTripleKatana'] = false,
-    ['CursedDualKatana'] = true
-}
-_G.GunSettings = {
-    ['Kabucha'] = true,
-    ['SerpentBow'] = true,
-    ['SoulGuitar'] = false
-}
-_G.FarmMastery_Settings = {
-    ['Melee'] = true,
-    ['Sword'] = true,
-    ['DevilFruits'] = true,
-    ['Select_Swords'] = {
-        ["AutoSettings"] = true,
-        ["ManualSettings"] = {
-            "Saber",
-            "Buddy Sword"
-        }
-    }
-}
-_G.Hop_Settings = {
-    ["Find Tushita"] = true
-}
-(getgenv()).key = "MARU0RMFMYMFPXOI6YYZUNUM";
-(getgenv()).id = "878872687286571018";
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruComkak/main/PCBit.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaCat-kaitunBF.lua"))()
+task.delay(6, function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Hzddprivate/Script/main/Blox%20Fruits/MoreConfig.lua"))()
+end)
