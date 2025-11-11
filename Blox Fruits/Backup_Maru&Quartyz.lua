6bc58376c1b4d2f248d888ddc92e3e658ca64071
Hzddprivate <148460926+Hzddprivate@users.noreply.github.com>
2025-11-11 18:37:39 +0700

    Update Backup_Maru&Quartyz.lua

--- Blox Fruits/Backup_Maru&Quartyz.lua
+++ Blox Fruits/Backup_Maru&Quartyz.lua
@@ -5,104 +5,36 @@ repeat task.wait() until game:IsLoaded()
 repeat task.wait() until game.Players
 repeat task.wait() until game.Players.LocalPlayer
 repeat task.wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui")
-_G.Team = "Pirate" -- Marine / Pirate
 getgenv().Script_Mode = "Kaitun_Script"
-_G.MainSettings = {
-    ["EnabledHOP"] = true,
-    ['FPSBOOST'] = true,
-    ["FPSLOCKAMOUNT"] = 15,
-    ['WhiteScreen'] = false,
-    ['CloseUI'] = false,
-    ["NotifycationExPRemove"] = true,
-    ['AFKCheck'] = 150,
-    ["LockFragments"] = 50000,
-    ["LockFruitsRaid"] = {
-        [1] = "Tiger-Tiger",
-        [2] = "Kitsune-Kitsune",
-        [3] = "Dragon-Dragon",
-        [4] = "Yeti-Yeti",
-        [5] = "Gas-Gas",
-        [6] = "Dough-Dough",
-        [7] = "Buddha-Buddha",
-        [8] = "Spirit-Spirit",
+_G.OneClickSettings = {
+    ["LockFruits"] = {
+        "Tiger-Tiger",
+        "Kitsune-Kitsune",
+        "Dragon-Dragon",
+        "Yeti-Yeti",
+        "Gas-Gas",
+        "Dough-Dough"
     },
-    }
-}
-_G.SharkAnchor_Settings = {
-    ["Enabled_Farm"] = false,
-    ['FarmAfterMoney'] = 2500000
-}
-_G.Quests_Settings = {        
-    ['Rainbow_Haki'] = true,
-    ["MusketeerHat"] = true,
-    ["PullLever"] = true,
-    ['DoughQuests_Mirror'] = {
-        ['Enabled'] = true,
-        ['UseFruits'] = true
-    }        
-}
-_G.Races_Settings = {
-    ['Race'] = {
-        ['EnabledEvo'] = true,
-        ["v2"] = true,
-        ["v3"] = true,
-        ["Races_Lock"] = {
-            ["Races"] = {
-                ["Mink"] = true,
-                ["Human"] = true,
-                ["Fishman"] = true
-            },
-            ["RerollsWhenFragments"] = 20000
-        }
-    }
-}
-_G.Fruits_Settings = {
-    ['Main_Fruits'] = {'Dough-Dough', 'Buddha-Buddha'},
-    ['Select_Fruits'] = {"Light-Light", "Dark-Dark", "Magma-Magma"}
-}
-_G.Settings_Melee = {
-    ['Superhuman'] = true,
-    ['DeathStep'] = true,
-    ['SharkmanKarate'] = true,
-    ['ElectricClaw'] = true,
-    ['DragonTalon'] = true,
-    ['Godhuman'] = true
-}
-_G.SwordSettings = {
-    ['Saber'] = true,
-    ["Pole"] = true,
-    ['MidnightBlade'] = true,
-    ['Shisui'] = true,
-    ['Saddi'] = false,
-    ['Wando'] = false,
-    ['Yama'] = true,
-    ['Rengoku'] = true,
-    ['Canvander'] = true,
-    ['BuddySword'] = true,
-    ['TwinHooks'] = true,
-    ['HallowScryte'] = true,
-    ['TrueTripleKatana'] = false,
-    ['CursedDualKatana'] = true
-}
-_G.GunSettings = {
-    ['Kabucha'] = true,
-    ['SerpentBow'] = true,
-    ['SoulGuitar'] = false
-}
-_G.FarmMastery_Settings = {
-    ['Melee'] = true,
-    ['Sword'] = true,
-    ['DevilFruits'] = true,
-    ['Select_Swords'] = {
-        ["AutoSettings"] = true,
-        ["ManualSettings"] = {
-            "Saber",
-            "Buddy Sword"
-        }
-    }
-}
-_G.Hop_Settings = {
-    ["Find Tushita"] = true
+    ["MainFruits"] = {
+        "Dough-Dough",
+        "Tiger-Tiger",
+        "Kitsune-Kitsune",
+        "Dragon-Dragon",
+        "Yeti-Yeti",
+        "Gas-Gas",
+        "Buddha-Buddha",
+        "Spirit-Spirit",
+        "Buddha-Buddha",
+        "T-rex-T-rex"
+    },
+    ["RegularFruits"] = {
+        "Flame-Flame",
+        "Magmag-Magma",
+        "Dark-Dark",
+        "Light-Light"
+    },
+    ["FragmentsCap"] = 70000,
+    ["Farm Shark Anchor"] = false
 }
 (getgenv()).key = "MARU0RMFMYMFPXOI6YYZUNUM";
 (getgenv()).id = "878872687286571018";
